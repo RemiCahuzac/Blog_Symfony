75c8383f13932b3f4dda9cca1e9cbfa084445547
Cahuzac Remi <cahuzac.remi@gmail.com>
2020-10-17 19:27:27 +0200

    Mise en place de Faker pour Catégories, Article et commentaires. 1 / 2

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,16 +12,42 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create('Fr_fr');
+
+        //Créer 3 Cat"gories Fakée
+        for($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph(3));
+            $manager->persist($category);
 
 
-        for($i= 1 ; $i <= 10 ; $i++){
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("htp///placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+            for ($j = 1; $j <= mt_rand(4,6); $j++) {
+                $article = new Article();
 
-            $manager->persist($article);
+                $content = '<p>'.join($faker->paragraph(5), '</p><p>'.'</p>');
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                for ($k = 1; $k <= mt_rand(4,10); $k++) {
+                    $comment = new Comment();
+
+                    $content = '<p>'.join($faker->paragraph(2), '</p><p>'.'</p>');
+
+
+
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt();
+                }
+            }
         }
         $manager->flush();
     }
